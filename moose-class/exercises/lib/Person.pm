78204667be8035e186f28ab73d48f82e6799408d
Yuval Kogman <nothingmuch@woobling.org>
2009-07-21 09:00:57 -0700

    typo

--- moose-class/exercises/lib/Person.pm
+++ moose-class/exercises/lib/Person.pm
@@ -1,4 +1,30 @@
 package Person;
 
+use Moose;
+
+use namespace::clean -except => "meta";
+
+has [qw(first_name last_name)] => (
+    is => "rw",
+    required => 1,
+);
+
+sub full_name {
+    my $self = shift;
+    $self->first_name . " " . $self->last_name;
+}
+
+override BUILDARGS => sub {
+    my ( $self, @args ) = @_;
+
+    if ( @args == 1 and ref $args[0] eq 'ARRAY' ) {
+        my %p; @p{qw(first_name last_name)} = @{ $args[0] };
+        return \%p;
+    } else {
+        return super;
+    }
+};
+
+__PACKAGE__->meta->make_immutable;
 
 1;
